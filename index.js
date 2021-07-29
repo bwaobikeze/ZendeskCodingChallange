1a45d07a4105aeb38801bbe1da48acc97c5f1679
bwaobikeze <bwaobikeze@hotmail.com>
2021-07-29 01:25:51 -0500

    listed all of the tickets information

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-let axios = require('axios');
+const axios = require('axios');
 
-let config = {
-  method: 'get',
-  url: 'https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json',
-  headers: { 
-    'Authorization': 'Basic YnJpYW4ud2FvYmlrZXplQGdtYWlsLmNvbTpLaW5kYm95MjBA', 
-    'Cookie': '__cfruid=c2d00345d80097a8018a3307ab6e5cc0abe6c1a2-1627511044'
-  }
-};
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+
+
+axios.get("https://jsonplaceholder.typicode.com/users").then(result => console.log(result.data)).catch(err => console.log(err))
+
+
+
