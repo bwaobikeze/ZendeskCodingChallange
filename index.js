e3ee3a9f8c743af8711035498323f04727e0a0b8
bwaobikeze <bwaobikeze@hotmail.com>
2021-07-30 03:05:32 -0500

    added user input

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 const axios = require('axios');
-const prompt = require('prompt-sync')({sigint: true});
+const readline = require('readline');
 
-// startRequest();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+ 
+rl.question(`Welcome to Your Zendesk Ticket Viewer Please Press 1 to Move foward: `, choice => {
+    if (choice == 1){
+        listTotaltickets();
+    }else
+    console.log("sorry see you again");
+    rl.close()
+  });
 
-// function startRequest(){
-//     let menuDes = prompt('Welcome To Your ticket viewer Please select 1 to view your ticket: ');
 
-//     switch(menuDes){
-//     case 1:
-//     listTotaltickets();
-//     break;
-//     case 2:
-//     console.log('See you soon');
-//     break;
-//     }
+// axios.get('https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json').then(result => console.log(result.data)).catch(err => console.log(err))
+// listTotaltickets();
 
+function listTotaltickets(){
+axios.get('https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json/', {
+  headers: {
+    'Authorization': 'Basic YnJpYW4ud2FvYmlrZXplQGdtYWlsLmNvbTpLaW5kYm95MjBA', 
+    'Cookie': '__cfruid=c2d00345d80097a8018a3307ab6e5cc0abe6c1a2-1627511044'
+  }
+})
+.then((res) => {
+  console.log(res.data)
+})
+.then((ticketChoice) =>{
+    rl.question(`Whitch ticket do you want view: `,ticketChoice=>{
+
+        showingTicket(ticketChoice);
     
-// }
-
-//axios.get('https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json').then(result => console.log(result.data)).catch(err => console.log(err))
-//listTotaltickets();
-
-// function listTotaltickets(){
-// axios.get('https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json/', {
-//   headers: {
-//     'Authorization': 'Basic YnJpYW4ud2FvYmlrZXplQGdtYWlsLmNvbTpLaW5kYm95MjBA', 
-//     'Cookie': '__cfruid=c2d00345d80097a8018a3307ab6e5cc0abe6c1a2-1627511044'
-//   }
-// })
-// .then((res) => {
-//   console.log(res.data)
-// })
-// .catch((error) => {
-//   console.error(error)
-// })
+        rl.close()
+    });
+})
+.catch((error) => {
+  console.error(error)
+})
 
 
-//}
-showingTicket(100);
+}
+// showingTicket(100);
 
 function showingTicket(ticketId){
 
@@ -55,4 +60,4 @@ function showingTicket(ticketId){
       })
       
 
-}
+ }
