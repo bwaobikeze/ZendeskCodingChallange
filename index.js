b80bac3ad8a65b0db8ec3834e6b9eed99e9207af
bwaobikeze <bwaobikeze@my.lonestar.edu>
2021-08-02 05:22:34 -0500

    fixed user input issue

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-const axios = require('axios');
-const pageTurn= require ('./pageChoice')
-const readline = require('readline');
+const axios = require("axios");
+const prompt = require("prompt-sync")({ sigint: true });
+const pageTurn = require("./pageChoice");
+//const readline = require("readline");
 
 //const prompt = require ('prompt');
 //let pageFlip =pageTurn.pageDececion();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
- 
-   
-rl.question(`Welcome to Your Zendesk Ticket Viewer Please Press 1 to Move foward: `, choice => {
-    if (choice == 1){
-        listTotaltickets();
-    }else
-    console.log("sorry see you again");
-    rl.close()
-  });
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false,
+// });
 
+BeginPro();
+
+function BeginPro() {
+  let choice = prompt(
+    "Welcome to Your Zendesk Ticket Viewer Please Press 1 to Move foward:  "
+  );
+
+  if (choice == 1) {
+    listTotaltickets();
+  } else console.log(choice);
+}
+
+// rl.question(
+//   `Welcome to Your Zendesk Ticket Viewer Please Press 1 to Move foward: `,
+//   (choice) => {
+//     if (choice == 1) {
+//       listTotaltickets();
+//     } else console.log("sorry see you again");
+//     rl.close();
+//   }
+// );
 
 // axios.get('https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json').then(result => console.log(result.data)).catch(err => console.log(err))
 // listTotaltickets();
 
-
-
-
 // in this function loop list of tickets 25 on each page
-// give option to next page 
+// give option to next page
 
-async function listTotaltickets(){
-
-
-    try {
-        let apiCal = await axios.get('https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json?page[size]=25', {
-              headers: {
-                'Authorization': 'Basic YnJpYW4ud2FvYmlrZXplQGdtYWlsLmNvbTpLaW5kYm95MjBA', 
-                'Cookie': '__cfruid=c2d00345d80097a8018a3307ab6e5cc0abe6c1a2-1627511044'
-              }});
-        console.log(apiCal.data);
-        pageTurn.pageDececion();
-    } catch (error) {
-        console.log(error.message)
-    }
-
-
+async function listTotaltickets() {
+  try {
+    let apiCal = await axios.get(
+      "https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json?page[size]=25",
+      {
+        headers: {
+          Authorization:
+            "Basic YnJpYW4ud2FvYmlrZXplQGdtYWlsLmNvbTpLaW5kYm95MjBA",
+          Cookie:
+            "__cfruid=c2d00345d80097a8018a3307ab6e5cc0abe6c1a2-1627511044",
+        },
+      }
+    );
+    console.log(apiCal.data);
+    pageTurn.pageDececion();
+  } catch (error) {
+    console.log(error.message);
+  }
 }
-
-
-
-
--- pageChoice.js
+++ pageChoice.js
@@ -1,25 +1,46 @@
-const readline = require('readline');
-const axios = require('axios');
+const readline = require("readline");
+const axios = require("axios");
+const prompt = require("prompt-sync")({ sigint: true });
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  input: process.stdin,
+  output: process.stdout,
+});
 
+function pageDececion() {
+  let choice = prompt(
+    "Would you like to turn the next page?\n Press 1 To Continue\n Press 2 To Move Back To Prev:\n "
+  );
 
-
-function pageDececion(){
-    rl.setPrompt('Would you like to turn the next page?\n Press 1 To Continue\n Press 2 To Move Back To Prev:\n');
-    rl.prompt();
-    rl.on('line',(secondChoice)=>{
-        if(secondChoice== 1){
-            console.log("hello");
+  if (choice == 1) {
+    axios
+      .get(
+        "https://zccbrianwaobikeze.zendesk.com/api/v2/tickets.json?page[size]=25&page[after]=eyJvIjoibmljZV9pZCIsInYiOiJhUmtBQUFBQUFBQUEifQ==",
+        {
+          headers: {
+            Authorization:
+              "Basic YnJpYW4ud2FvYmlrZXplQGdtYWlsLmNvbTpLaW5kYm95MjBA",
+            Cookie:
+              "__cfruid=c2d00345d80097a8018a3307ab6e5cc0abe6c1a2-1627511044",
+          },
         }
-        rl.close();
-    })
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  //   rl.setPrompt(
+  //     "Would you like to turn the next page?\n Press 1 To Continue\n Press 2 To Move Back To Prev:\n"
+  //   );
+  //   rl.prompt();
+  //   rl.on("line", (secondChoice) => {
+  //     if (secondChoice == 1) {
+  //       console.log("hello");
+  //     }
+  //   });
+}
 
-    }
-
-    
-
-    exports.pageDececion = pageDececion;
+exports.pageDececion = pageDececion;
